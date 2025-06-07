99e1c85fa813a41d5af73ed6929c7b52aa39e984
Sohei Yamazaki <sohyamaz@c3r6s8.42tokyo.jp>
2025-06-07 21:51:14 +0900

    refactor(bresenham.c):separate func and bug fixed

--- plac_home/bresenham.c
+++ plac_home/bresenham.c
@@ -6,7 +6,7 @@
 /*   By: sohyamaz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/01 10:47:23 by sohyamaz          #+#    #+#             */
-/*   Updated: 2025/06/07 16:25:08 by sohyamaz         ###   ########.fr       */
+/*   Updated: 2025/06/07 21:48:33 by sohyamaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,43 +18,63 @@ int	set_sign(int start, int goal)
 
 	sign = 0;
 	if (start < goal)
-		sign= 1;
+		sign = 1;
 	else
 		sign = -1;
 	return (sign);
 }
 
-void	bresenham(t_structs *val, t_isol *st, t_isol *gl, int color)
+int	set_calc(t_structs *val, t_isom *st, t_isom *gl)
+{
+	if (val == NULL || st == NULL || gl == NULL)
+		return (ERR_NULL_VALUE_DETECTED);
+	val->calc->delta_x = abs(gl->x - st->x);
+	val->calc->delta_y = abs(gl->y - st->y);
+	val->calc->err = val->calc->delta_x - val->calc->delta_y;
+	val->calc->sign_x = set_sign(st->x, gl->x);
+	val->calc->sign_y = set_sign(st->y, gl->y);
+	return (0);
+}
+
+int	err_check(t_calc *calc, int *x, int *y, int err2)
+{
+	if (calc == NULL)
+		return (ERR_NULL_VALUE_DETECTED);
+	if (err2 >= -calc->delta_y)
+	{
+		calc->err = calc->err + calc->delta_y;
+		*x = *x + calc->sign_x;
+	}
+	if (err2 < calc->delta_x)
+	{
+		calc->err = calc->err + calc->delta_x;
+		*y = *y + calc->sign_y;
+	}
+	return (0);
+}
+
+int	bresenham(t_structs *val, t_isom *st, t_isom *gl, int color)
 {
 	int	x;
 	int	y;
-	int err2;
+	int	err2;
+	int	error;
 
-	val->calc->delta_x = abs(gl->x - st->x);
-	val->calc->delta_y = -abs(gl->y - st->y);
-	val->calc->err = delta_x + delta_y;
-	val->calc->sign_x = set_sign(st->x, gl->x);
-	val->calc->sign_y = set_sign(st->y, gl->y);
 	x = st->x;
 	y = st->y;
-	pixel_put(val, st->x, st->y, color);
+	error = 0;
+	err2 = set_calc(val, st, gl);
+	if (err2 != 0)
+		return (err2);
 	while (1)
 	{
 		pixel_put(val, x, y, color);
 		if (x == gl->x && y == gl->y)
 			break ;
-		err2 = err * 2;
-		if (val->calc->delta_y <= err2)
-		{	
-			val->calc->err = val->calc->err + val->calc->delta_y;
-			x = x + val->calc->sign_x;
-		}
-		if (err2 <= val->calc->delta_x)
-		{
-			val->calc->err = val->calc->err + val->calc->delta_x;
-			y = y + val->calc->sign_y;
-		}
+		err2 = val->calc->err * 2;
+		error = err_check(val->calc, &x, &y, err2);
+		if (error == ERR_NULL_VALUE_DETECTED)
+			return (error);
 	}
-	pixel_put(val, gl->x, gl->y, color);
-	return ;
+	return (0);
 }
