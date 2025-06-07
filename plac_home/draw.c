48513966fe61dc4a73db34ebe8bd4a8fb2946a56
Sohei Yamazaki <sohyamaz@c3r6s8.42tokyo.jp>
2025-06-07 22:19:24 +0900

    feat(draw.c):add line draw funcs

--- plac_home/draw.c
+++ plac_home/draw.c
@@ -6,31 +6,57 @@
 /*   By: sohyamaz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/01 10:46:52 by sohyamaz          #+#    #+#             */
-/*   Updated: 2025/06/07 15:50:05 by sohyamaz         ###   ########.fr       */
+/*   Updated: 2025/06/07 22:16:46 by sohyamaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "prot.h"
 
+int	draw_loop(t_structs *val, int vt, int color)
+{
+	int	hr;
+	int	error;
+
+	hr = 0;
+	if (val == NULL || val->map == NULL)
+		return (ERR_NULL_VALUE_DETECTED);
+	while (hr < val->map->width)
+	{
+		if (hr < val->map->width - 1)
+		{
+			error = bresenham(val, val->isol[vt][hr], \
+					val->isol[vt][hr + 1], color);
+			if (error != 0)
+				return (error);
+		}
+		if (vt < val->map->height - 1)
+		{
+			error = bresenham(val, val->isol[vt][hr], \
+					val->isol[vt + 1][hr], color);
+			if (error != 0)
+				return (error);
+		}
+		hr++;
+	}
+	return (0);
+}
+
 void	draw_line(t_structs *val)
 {
 	int	vt;
-	int	hr;
-	int color;
+	int	color;
+	int	error;
 
 	color = 0xFFFFFFFF;
 	vt = 0;
+	error = 0;
+	if (val == NULL || val->map == NULL)
+		error_exit(val, ERR_NULL_VALUE_DETECTED);
 	while (vt < val->map->height)
 	{
-		hr = 0;
-		while (hr < val->map->width)
-		{
-			if (hr < val->map->width - 1)
-				bresenham(val, val->isol[vt][hr], val->isol[vt][hr + 1], color);
-			if (vt < val->map->height - 1)
-				bresenham(val, val->isol[vt][hr], val->isol[vt + 1][hr], color);
-			hr++;
-		}
+		error = draw_loop(val, vt, color);
+		if (error != 0)
+			error_exit(val, error);
 		vt++;
 	}
 	return ;
