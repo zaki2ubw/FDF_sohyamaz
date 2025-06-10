97f004ea02ac7097b72e10be56882a2eab4b11ea
Sohei Yamazaki <sohyamaz@c3r6s4.42tokyo.jp>
2025-06-10 22:06:46 +0900

    feat(event.c):add Window close func

--- plac_home/event.c
+++ plac_home/event.c
@@ -6,25 +6,31 @@
 /*   By: sohyamaz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/01 10:47:40 by sohyamaz          #+#    #+#             */
-/*   Updated: 2025/06/08 13:45:38 by sohyamaz         ###   ########.fr       */
+/*   Updated: 2025/06/10 22:02:07 by sohyamaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "prot.h"
 
-int	close_window(int keycode, t_structs *val)
+int	close_window_esc(int keycode, t_structs *val)
 {
 	if (val == NULL)
-		exit (0);
+		exit (ERR_NULL_VALUE_DETECTED);
 	if (keycode == KEY_ESC)
 	{
 		mlx_destroy_image(val->var->mlx, val->image->img);
 		mlx_destroy_window(val->var->mlx, val->var->win);
 		mlx_destroy_display(val->var->mlx);
-		free(val->image);
-		free(val->var->mlx);
-		free(val->var);
+		free_val(&val);
 		exit (0);
 	}
 	return (0);
+}
+
+int	close_window_cross(t_structs *val)
+{
+	if (val == NULL)
+		exit (ERR_NULL_VALUE_DETECTED);
+	free_val(&val);
+	exit (0);
 }
