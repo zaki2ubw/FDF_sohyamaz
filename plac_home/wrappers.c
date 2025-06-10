e445c4782a22dbde0612e17334368c7b9739a9ae
Sohei Yamazaki <sohyamaz@c3r6s4.42tokyo.jp>
2025-06-10 21:51:12 +0900

    style(wrappers.c):style(wrappers.c):fix it based on norminette

--- plac_home/wrappers.c
+++ plac_home/wrappers.c
@@ -6,17 +6,19 @@
 /*   By: sohyamaz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 21:39:53 by sohyamaz          #+#    #+#             */
-/*   Updated: 2025/06/10 21:39:55 by sohyamaz         ###   ########.fr       */
+/*   Updated: 2025/06/10 21:50:03 by sohyamaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "prot.h"
 
-int draw_line_wrapper(void *ptr)
+int	draw_line_wrapper(void *ptr)
 {
-    if (ptr == NULL)
-        return (ERR_NULL_VALUE_DETECTED);
-    t_structs *val = (t_structs *)ptr;
-    draw_line(val);
-    return (0);
+	t_structs	*val;
+
+	if (ptr == NULL)
+		return (ERR_NULL_VALUE_DETECTED);
+	val = (t_structs *)ptr;
+	draw_line(val);
+	return (0);
 }
