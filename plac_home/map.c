f75969dd3f294e5fa5c96816febe806fa060dd1c
Sohei Yamazaki <sohyamaz@c4r3s5.42tokyo.jp>
2025-06-15 19:29:58 +0900

    feat(check_map_size):change logic from cutoff large size to get maximam length

--- plac_home/map.c
+++ plac_home/map.c
@@ -6,7 +6,7 @@
 /*   By: sohyamaz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/01 10:44:30 by sohyamaz          #+#    #+#             */
-/*   Updated: 2025/06/10 22:45:42 by sohyamaz         ###   ########.fr       */
+/*   Updated: 2025/06/15 19:15:29 by sohyamaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,50 +17,27 @@ int	count_column(char *line, char cut)
 	int	count;
 	int	column;
 
-	column = 0;
+	column = 1;
 	count = 0;
 	if (line == NULL)
 		return (0);
 	while (line[count] != '\0' && line[count] != '\n')
 	{
-		if (line[count] != cut)
+		if (line[count] == cut)
 			column++;
 		count++;
 	}
 	return (column);
 }
 
-int	set_width(t_map **map, int fd)
-{
-	char	*line;
-	int		count;
-	int		column;
-
-	column = 0;
-	count = 0;
-	if (*map == NULL)
-		return (ERR_NULL_VALUE_DETECTED);
-	map->width = ft_calloc(sizeof(int), map->width);
-	if (map->width == NULL)
-		return (ERR_ALLOC_FAILED);
-	while (count < map->height)
-	{
-		line = get_next_line(fd);
-		if (line == NULL)
-			break ;
-		map->width[count] = count_column(line, ' ');
-		free(line);
-		count++;
-	}
-	return (0);
-}
-
-int	set_row(t_map **map, int fd)
+int	set_height(t_map **map, int fd)
 {
 	char	*line;
 	int		row;
+	int		column;
 
 	row = 0;
+	(*map)->width = 0;
 	if (*map == NULL)
 		return (ERR_NULL_VALUE_DETECTED);
 	while (1)
@@ -68,6 +45,9 @@ int	set_row(t_map **map, int fd)
 		line = get_next_line(fd);
 		if (line == NULL)
 			break ;
+		column = count_column(line, ' ');
+		if ((*map)->width < column)
+			(*map)->width = column;
 		free(line);
 		row++;
 	}
@@ -89,11 +69,6 @@ void	check_map_size(t_structs *val, char *file, t_map **map)
 	close(fd);
 	if (error != 0)
 		error_exit(&val, error);
-	fd = open(file, O_RDONLY);
-	error = set_width(map, fd);
-	close(fd);
-	if (error != 0)
-		error_exit(&val, error);
 	return ;
 }
 
@@ -104,7 +79,7 @@ int	convert_map(t_map **map, char **args, int count)
 	i = 0;
 	if (*map == NULL)
 		return (ERR_NULL_VALUE_DETECTED);
-	while (i < (*map)->width)
+	while (args[i] != NULL && i < (*map)->width)
 	{
 		(*map)->z_map[count][i] = ft_atoi(args[i]);
 		i++;
@@ -165,13 +140,10 @@ void	read_map(t_structs *val, char *file, t_map **map)
 
 	if (val == NULL || file == NULL || *map == NULL)
 		error_exit(&val, ERR_NULL_VALUE_DETECTED);
-	fd = open(file, O_RDONLY);
 	error = init_z_map(map);
 	if (error != 0)
-	{
-		close (fd);
 		error_exit(&val, error);
-	}
+	fd = open(file, O_RDONLY);
 	error = set_map(map, fd);
 	if (error != 0)
 	{
